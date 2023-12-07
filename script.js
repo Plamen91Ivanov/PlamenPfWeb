18f486a0d69164519cbc7d6291de5216e6338b7b
PlamenIvanov <plamen91ivanov@abv.bg>
2023-12-07 10:44:57 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+   function ChangeBackgroundColor() {
+  document.getElementById("first-icon").style.backgroundColor = "#83f676";
+}
+function RemoveBackgroundColor(){
+  document.getElementById("first-icon").style.backgroundColor = "transparent";
+}
+function ChangeBackgroundColorSecond() {
+  document.getElementById("second-icon").style.backgroundColor = "#83f676";
+}
+function RemoveBackgroundColorSecond(){
+  document.getElementById("second-icon").style.backgroundColor = "transparent";
+}
+function AddVisability() {
+  document.getElementById("project-img").style.visibility = "visible";
+}
+function RemoveVisability(){
+  document.getElementById("project-img").style.visibility = "hidden";
+}
+function AddGitBackgroundColorFirst() {
+    document.getElementById("first-git-icon").style.backgroundColor =  "#83f676";
+}
+function RemoveGitBackgroundColorFirst() {
+    document.getElementById("first-git-icon").style.backgroundColor = "transparent";
+}
+function AddGitBackgroundColorFourth()
+{
+    document.getElementById("fourth-git-icon").style.backgroundColor = "#83f676";
+}
+function RemoveGitBackgroundColorFourth()
+{
+    document.getElementById("fourth-git-icon").style.backgroundColor = "transparent"
+}
